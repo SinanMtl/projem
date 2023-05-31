d535152ccc84830b3faa7d597c61596ab4280570
Sinan Mutlu <sinanmutlu@teknasyon.com>
2023-05-31 19:42:54 +0300

    ilk commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// Ders 28: Versiyon Kontrolü Nasıl Yapılır?
+
+// - Repository
+
+/*
+ - Git
+ - SVN
+ - BitKeepr
+ - Mercruial
+*/
+
+/*
+  - Local
+  - Centralized
+  - Distributed
+*/
+
+// Git
+// - GitHub -> 
+// - GitLab ->
+// - Bitbucket ->
+
+/*
+ - Repository
+ - Working Tree
+ - Commit
+ - Stage Area
+ - Branch
+*/
+
+/*
+ pwd: (MacOS, Linux) Terminal üzerinde bulunduğunuz dizini adresini verir. Win için cd
+ ls: (MacOS, Linux) Bulunduğunuz dizindeki dosya ve dizinleri gösterir.
+ dir /s: (Win) Bulunduğunuz dizindeki dosya ve dizinleri gösterir.
+ cd: Ulaşmak istediğiniz dizine götürür. Kullanım: cd ./data
+ clear: (MacOS, Linux) Terminali temizler
+ cls: (Win) Terminali temizler
+*/
+
+/*
+  git clone <uzak_sunucu_adresi>
+  git add -A
+  git status
+  git commit
+*/
